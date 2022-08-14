6bcab085ee89c232e3780a6bae2040cc52959632
CruddyShaun <108172682+CruddyShaun@users.noreply.github.com>
2022-08-14 10:37:55 +0200

    Add files via upload

---
+++ evenoneven.py
@@ -0,0 +1,14 @@
+print("Even of oneven nummers")
+print("\n")
+
+x =  input("Geef uw nummer\n")
+x = float(x)
+
+if x % 2 == 0 and x > 0:
+
+    print("Even")
+    
+else:
+    print("Oneven")
+    
+
